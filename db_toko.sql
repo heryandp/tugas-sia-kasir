5c3bbb0d279bb1f4ff458871cf10a619fc570142
heryandp <haxornesia@gmail.com>
2023-12-19 11:46:45 +0700

    update db

--- db_toko.sql
+++ db_toko.sql
@@ -1,20 +1,11 @@
-/*
-SQLyog Professional v12.5.1 (64 bit)
-MySQL - 10.6.11-MariaDB-log : Database - db_toko
-*********************************************************************
-*/
+-- Database export via SQLPro (https://www.sqlprostudio.com/allapps.html)
+-- Exported by heryan at 19-12-2023 11:46.
+-- WARNING: This file may contain descructive statements such as DROPs.
+-- Please ensure that you are running the script at the proper location.
 
-/*!40101 SET NAMES utf8 */;
 
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `barang` */
-
-DROP TABLE IF EXISTS `barang`;
-
+-- BEGIN TABLE barang
+DROP TABLE IF EXISTS barang;
 CREATE TABLE `barang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
@@ -28,55 +19,58 @@ CREATE TABLE `barang` (
   `tgl_input` varchar(255) NOT NULL,
   `tgl_update` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
-/*Data for the table `barang` */
+-- Inserting 3 rows into barang
+-- Insert batch #1
+INSERT INTO barang (id, id_barang, id_kategori, nama_barang, merk, harga_beli, harga_jual, satuan_barang, stok, tgl_input, tgl_update) VALUES
+(1, 'ATK1', 1, 'Pulpen', 'Standar', '1500', '1700', 'PCS', '55', '19 December 2023, 11:39', NULL),
+(2, 'ATK2', 1, 'Pensil', 'Faber Castell', '1600', '1700', 'PCS', '88', '19 December 2023, 11:39', NULL),
+(4, 'VOC3', 8, 'Diamond 50', 'Mobile Legend', '100000', '105000', 'PCS', '55', '19 December 2023, 11:41', NULL);
 
-insert  into `barang`(`id`,`id_barang`,`id_kategori`,`nama_barang`,`merk`,`harga_beli`,`harga_jual`,`satuan_barang`,`stok`,`tgl_input`,`tgl_update`) values 
-(1,'BR001',1,'Pensil','Fabel Castel','1500','3000','PCS','98','6 October 2020, 0:41',NULL),
-(2,'BR002',5,'Sabun','Lifeboy','1800','3000','PCS','38','6 October 2020, 0:41','6 October 2020, 0:54'),
-(3,'BR003',1,'Pulpen','Standard','1500','2000','PCS','70','6 October 2020, 1:34',NULL);
+-- END TABLE barang
 
-/*Table structure for table `kategori` */
-
-DROP TABLE IF EXISTS `kategori`;
-
+-- BEGIN TABLE kategori
+DROP TABLE IF EXISTS kategori;
 CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
+  `kode_kategori` varchar(30) NOT NULL,
   `nama_kategori` varchar(255) NOT NULL,
   `tgl_input` varchar(255) NOT NULL,
   PRIMARY KEY (`id_kategori`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
-/*Data for the table `kategori` */
+-- Inserting 6 rows into kategori
+-- Insert batch #1
+INSERT INTO kategori (id_kategori, kode_kategori, nama_kategori, tgl_input) VALUES
+(1, 'ATK', 'ATK', '7 May 2017, 10:23'),
+(5, 'SA', 'Sabun', '7 May 2017, 10:28'),
+(6, 'SN', 'Snack', '6 October 2020, 0:19'),
+(7, 'MIN', 'Minuman', '6 October 2020, 0:20'),
+(8, 'VOC', 'Voucher', '19 December 2023, 9:55'),
+(9, 'PLS', 'Pulsa', '19 December 2023, 10:09');
 
-insert  into `kategori`(`id_kategori`,`nama_kategori`,`tgl_input`) values 
-(1,'ATK','7 May 2017, 10:23'),
-(5,'Sabun','7 May 2017, 10:28'),
-(6,'Snack','6 October 2020, 0:19'),
-(7,'Minuman','6 October 2020, 0:20');
+-- END TABLE kategori
 
-/*Table structure for table `login` */
-
-DROP TABLE IF EXISTS `login`;
-
+-- BEGIN TABLE login
+DROP TABLE IF EXISTS login;
 CREATE TABLE `login` (
   `id_login` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
   `pass` char(32) NOT NULL,
   `id_member` int(11) NOT NULL,
   PRIMARY KEY (`id_login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `login` */
+-- Inserting 1 row into login
+-- Insert batch #1
+INSERT INTO login (id_login, user, pass, id_member) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
 
-insert  into `login`(`id_login`,`user`,`pass`,`id_member`) values 
-(1,'admin','202cb962ac59075b964b07152d234b70',1);
+-- END TABLE login
 
-/*Table structure for table `member` */
-
-DROP TABLE IF EXISTS `member`;
-
+-- BEGIN TABLE member
+DROP TABLE IF EXISTS member;
 CREATE TABLE `member` (
   `id_member` int(11) NOT NULL AUTO_INCREMENT,
   `nm_member` varchar(255) NOT NULL,
@@ -86,17 +80,17 @@ CREATE TABLE `member` (
   `gambar` text NOT NULL,
   `NIK` text NOT NULL,
   PRIMARY KEY (`id_member`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `member` */
+-- Inserting 1 row into member
+-- Insert batch #1
+INSERT INTO member (id_member, nm_member, alamat_member, telepon, email, gambar, NIK) VALUES
+(1, 'Fauzan Falah', 'uj harapan', '081234567890', 'example@gmail.com', 'unnamed.jpg', '12314121');
 
-insert  into `member`(`id_member`,`nm_member`,`alamat_member`,`telepon`,`email`,`gambar`,`NIK`) values 
-(1,'Fauzan Falah','uj harapan','081234567890','example@gmail.com','unnamed.jpg','12314121');
+-- END TABLE member
 
-/*Table structure for table `nota` */
-
-DROP TABLE IF EXISTS `nota`;
-
+-- BEGIN TABLE nota
+DROP TABLE IF EXISTS nota;
 CREATE TABLE `nota` (
   `id_nota` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
@@ -106,14 +100,22 @@ CREATE TABLE `nota` (
   `tanggal_input` varchar(255) NOT NULL,
   `periode` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_nota`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
-/*Data for the table `nota` */
+-- Inserting 6 rows into nota
+-- Insert batch #1
+INSERT INTO nota (id_nota, id_barang, id_member, jumlah, total, tanggal_input, periode) VALUES
+(1, 'BR002', 1, '5', '15000', '19 December 2023, 9:07', '12-2023'),
+(2, 'BR002', 1, '5', '15000', '19 December 2023, 9:07', '12-2023'),
+(3, 'BR002', 1, '6', '18000', '19 December 2023, 9:51', '12-2023'),
+(4, 'BR002', 1, '6', '18000', '19 December 2023, 9:51', '12-2023'),
+(5, 'BR002', 1, '6', '18000', '19 December 2023, 9:51', '12-2023'),
+(6, 'BR002', 1, '6', '18000', '19 December 2023, 9:51', '12-2023');
 
-/*Table structure for table `penjualan` */
+-- END TABLE nota
 
-DROP TABLE IF EXISTS `penjualan`;
-
+-- BEGIN TABLE penjualan
+DROP TABLE IF EXISTS penjualan;
 CREATE TABLE `penjualan` (
   `id_penjualan` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
@@ -122,14 +124,16 @@ CREATE TABLE `penjualan` (
   `total` varchar(255) NOT NULL,
   `tanggal_input` varchar(255) NOT NULL,
   PRIMARY KEY (`id_penjualan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-/*Data for the table `penjualan` */
+-- Table penjualan contains no data. No inserts have been genrated.
+-- Inserting 0 rows into penjualan
 
-/*Table structure for table `toko` */
 
-DROP TABLE IF EXISTS `toko`;
+-- END TABLE penjualan
 
+-- BEGIN TABLE toko
+DROP TABLE IF EXISTS toko;
 CREATE TABLE `toko` (
   `id_toko` int(11) NOT NULL AUTO_INCREMENT,
   `nama_toko` varchar(255) NOT NULL,
@@ -137,13 +141,12 @@ CREATE TABLE `toko` (
   `tlp` varchar(255) NOT NULL,
   `nama_pemilik` varchar(255) NOT NULL,
   PRIMARY KEY (`id_toko`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `toko` */
+-- Inserting 1 row into toko
+-- Insert batch #1
+INSERT INTO toko (id_toko, nama_toko, alamat_toko, tlp, nama_pemilik) VALUES
+(1, 'CV Daruttaqwa', 'Ujung Harapan', '081234567890', 'Fauzan Falah');
 
-insert  into `toko`(`id_toko`,`nama_toko`,`alamat_toko`,`tlp`,`nama_pemilik`) values 
-(1,'CV Daruttaqwa','Ujung Harapan','081234567890','Fauzan Falah');
+-- END TABLE toko
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
